12c50a56ef779557536a42cfe463e3fcdb60ca52
Randa Fahmy <randa.omarfahmy@gmail.com>
2018-03-18 16:27:59 +0200

    crash fix

--- app/src/main/java/ocs/com/dr_tips/fragment/LoginFragment.java
+++ app/src/main/java/ocs/com/dr_tips/fragment/LoginFragment.java
@@ -34,7 +34,7 @@ public class LoginFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
         ButterKnife.bind(this, view);
         setListeners();
         return view;
