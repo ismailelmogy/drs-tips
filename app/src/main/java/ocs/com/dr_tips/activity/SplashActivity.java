c6b4bb151caf944b9fa0cfd2665a33fb499cdc31
Asmaa Alamrawy <aalamrawy@overcoffeesolutions.com>
2018-03-18 17:17:57 +0200

    Splash screen : add logo , navigate to login activity , set splash activity noHistory to true

--- app/src/main/java/ocs/com/dr_tips/activity/SplashActivity.java
+++ app/src/main/java/ocs/com/dr_tips/activity/SplashActivity.java
@@ -1,7 +1,11 @@
 package ocs.com.dr_tips.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import ocs.com.dr_tips.R;
 
@@ -9,7 +13,23 @@ public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_splash);
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+
+            }
+        }.start();
     }
 }
