7f5b807bc6a053cf23e9b9c1adc57b91ccb2178e
Yassmen <yassmenhassan505@gmail.com>
2018-04-18 13:08:42 +0200

    settings and logout

--- app/src/main/java/ocs/com/dr_tips/fragment/SettingsFragment.java
+++ app/src/main/java/ocs/com/dr_tips/fragment/SettingsFragment.java
@@ -1,16 +1,42 @@
 package ocs.com.dr_tips.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ocs.com.dr_tips.R;
+import ocs.com.dr_tips.activity.LoginActivity;
 
 public class SettingsFragment extends DrsTipsBaseFragment {
+    @BindView(R.id.ic_change_pass)
+    ImageView changePasswordIc;
+    @BindView(R.id.ic_change_lang)
+    ImageView changeLanguageIc;
+    @BindView(R.id.ic_logout)
+    ImageView logoutIc;
+    @BindView(R.id.ic_payment_info)
+    ImageView paymentInfoIc;
+    @BindView(R.id.ic_contacr_us)
+    ImageView contactUsIc;
+
+    @OnClick(R.id.ic_logout)
+    void Logout(){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getActivity(), LoginActivity.class));
+        getActivity().finish();
+    }
+
     public SettingsFragment() {
         // Required empty public constructor
     }
@@ -27,7 +53,9 @@ public class SettingsFragment extends DrsTipsBaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view= inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this,view);
+        return view;
     }
 
 }
